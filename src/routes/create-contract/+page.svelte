3f5ba8504160a0ab9417a3a6fbfc47d84475aa0f
Alex Lin <38541529+alxn3@users.noreply.github.com>
2023-09-16 00:58:12 -0400

    Fix ids

--- src/routes/create-contract/+page.svelte
+++ src/routes/create-contract/+page.svelte
@@ -9,9 +9,9 @@
 		<input type="text" name="title" id="title" placeholder="Title" />
 		<label for="description">Background and Due Diligence</label>
 		<textarea name="description" id="description" placeholder="Desciption" cols="30" rows="10"  />
-		<label for="Starting Price">Starting Price (min 10)</label>
+		<label for="starting-price">Starting Price (min 10)</label>
 		<input type="number" name="starting-price" id="starting-price" min="10" placeholder="10" />
-		<label for="Volume">Volume (min 100)</label>
+		<label for="volume">Volume (min 100)</label>
 		<input type="number" name="volume" id="volume" min="100" placeholder="100" />
 		<Button class="w-fit p-2 text-2xl" type="submit">Create Contract</Button>
 	</form>
