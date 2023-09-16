ecbca25e224e1ca99e87cf66dc88d0e3360ba4cd
Alex Lin <38541529+alxn3@users.noreply.github.com>
2023-09-15 21:47:51 -0400

    Add font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,21 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 </script>
 
 <slot />
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link href="https://fonts.googleapis.com/css?family=Inter" rel="stylesheet" />
+
+	<title>VTHacks - Trade on your friends!</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta
+		name="description"
+		content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, voluptatum."
+	/>
+	<meta
+		name="keywords"
+		content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus excepturi, blanditiis nihil voluptatum reprehenderit nulla reiciendis unde illo dicta sapiente."
+	/>
+</svelte:head>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+import defaultTheme from 'tailwindcss/defaultTheme';
 
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		fontFamily: {
+			sans: ['Inter', ...defaultTheme.fontFamily.sans]
+		},
+		extend: {}
+	},
+	plugins: []
+};
