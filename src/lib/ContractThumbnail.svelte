1c5e319bd0813e2948aab9b242904f8ca188d014
Alex Lin <38541529+alxn3@users.noreply.github.com>
2023-09-16 13:53:39 -0400

    Make href

--- src/lib/ContractThumbnail.svelte
+++ src/lib/ContractThumbnail.svelte
@@ -2,13 +2,15 @@
 	export let title = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.';
 	export let endDate: Date = new Date();
 	import BelieveCope from './BelieveCope.svelte';
+	export let href: string = '#';
 </script>
 
-<button
+<a
 	class="text-left flex flex-col justify-between border-2 p-3 text-gray-300 bg-black font-thin rounded-xl text-lg h-full border-neutral-700"
+	{href}
 	on:click
 >
 	<BelieveCope />
 	<p class="text-white font-light overflow-y-auto">{title}</p>
 	<p>end date: {endDate.toLocaleDateString()}</p>
-</button>
+</a>
